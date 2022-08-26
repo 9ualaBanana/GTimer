b1bad94f02340c153613c086e87c05436c64df58
GIBSON <9ualabanana@gmail.com>
2022-08-26 12:18:26 +0300

    Turn Interval to record and add Default property.

--- GTimer/Interval.cs
+++ GTimer/Interval.cs
@@ -3,10 +3,15 @@
 /// <summary>
 /// Represents a time interval that can be implicitly casted from/to <see cref="double"/> or <see cref="TimeSpan"/>.
 /// </summary>
-public readonly struct Interval
+public record Interval
 {
     readonly double _asMilliseconds;
     readonly TimeSpan _asTimeSpan;
+
+    /// <summary>
+    /// Initializes interval value compliant with the initial value of <see cref="Timer.Interval"/>.
+    /// </summary>
+    public static Interval Default => new(100d);
 
     Interval(double value)
     {
@@ -18,7 +23,7 @@ public readonly struct Interval
     public static implicit operator Interval(TimeSpan storageTime) => new(storageTime.TotalMilliseconds);
     public static implicit operator TimeSpan(Interval storageTime) => storageTime._asTimeSpan;
 
-    public static implicit operator Interval(double milliseconds) => new Interval(milliseconds);
+    public static implicit operator Interval(double milliseconds) => new(milliseconds);
     public static implicit operator double(Interval storageTime) => storageTime._asMilliseconds;
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
